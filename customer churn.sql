92f57093575a494a3de9b319ac19a6011fb2ad1a
SIDHARTH SINGH <sidhuking1002@gmail.com>
2025-01-26 16:53:28 +0530

    Update customer churn.sql

--- customer churn.sql
+++ customer churn.sql
@@ -1,2 +1,5 @@
 create view vw_ChurnData as
-    select * from chur
+    select * from prod_Churn where Customer_Status in ('Churned','Stayed')
+
+create view vw_JoinData as 
+    select * from prod_Churn where Customer_Status = 'Joined'
